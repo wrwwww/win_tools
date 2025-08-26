f9b001bf6631105b9cdc85298fc6b437466bfb7f
mailang <2435960154@qq.com>
2025-08-26 09:39:48 +0800

    添加程序图标

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    // 设置 Windows 应用图标
+    #[cfg(windows)]
+    {
+        embed_resource::compile("ui/icon/icon.rc", embed_resource::NONE);
+    }
 }
